dbeac31c5e61283d66c06a549f336cfd720d88d2
AASB7 <wwwwbasingab@gmail.com>
2023-06-12 20:34:52 +0530

    Build basic project's structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Navbar from '@/components/Navbar'
+
 
 export default function Home() {
   return (
     <>
     <h1>Welcome</h1>
+    <Navbar/>
     </>
   )
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+function Navbar() {
+  return (
+    <div className=" bg-red-700 ">Navbar</div>
+  )
+}
+
+export default Navbar
