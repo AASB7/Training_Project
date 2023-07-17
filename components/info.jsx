1337278aacea3fdba3e2653dfdfac1a0deeee8de
aryam alharbi <aryamsultan45a@gmail.com>
2023-07-17 20:17:07 +0300

    project

---
+++ components/info.jsx
@@ -0,0 +1,31 @@
+export default function title() {
+  return (
+    <>
+    <title>hoadef</title>
+    
+      <section  className="nav1">
+      <nav className="navbar">
+        <ul>
+          <li> GPT-3 </li>
+          <li> HOME </li>
+          <li>what is GPT?</li>
+          <li>open Al </li>
+          <li>case studies </li>
+          <li>Library</li>
+        </ul>
+      </nav>
+      <button > sign in</button>
+      <button>login </button>
+    </section>
+    <div>
+      <h2   className="part1" >Let's Build something<br></br> amazing with GPT-3 <br>openAl</br> </h2>
+      <p>Yet bad any for travelling assistance indulgence unpleassing<br> not throughts all exercise blessing way </br>
+       everything joy altereation boisterous the attachment  party<br> we years to order allow asked of.</br> </p>
+    </div>
+
+    <button className="email">enter your email </button>
+    <button className="Get">Get started </button>
+    </>
+  )
+}
+export default function title()
