f1cc28e6c78f7a7327027ce6c93c0e96c1c0514e
HanenAljadani <haneenabdullah66@hotmail.com>
2023-07-19 23:43:23 +0300

    Create Gpt info component ,and add color to the text and background in page.jsx

---
+++ components/GptSection.jsx
@@ -0,0 +1,42 @@
+function GptSection() {
+  return (
+    <>
+    <div className="m-10  inline-block  bg-gradient-to-br from-color-blog to-blue-400">
+
+    <span className="flex justify-between">
+    <div className="pl-8 pt-8 text-3xl font-bold text-white w-1/2">What is GPT-3</div>
+    
+    <div className="pl-3 pt-9 pr-7 text-xs w-1/2 text-color-text">We so opinion friends me message as delight. 
+    Whole front do of plate heard oh ought. His defective nor convinced residence own. 
+    Connection has put impossible own apartments boisterous.
+     At jointure ladyship an insisted so humanity he. 
+     Friendly bachelor entrance to on by.</div>
+    </span>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <span className="p-8 flex justify-between">
+    <h3 className=" text-3xl bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text text-transparent w-1/3">The possibilities are beyond your imagination</h3>
+    <h6 className=" text-orange-400 text-sm ">Explore The Library</h6>
+    </span>
+    
+    <br/>
+    <br/>
+    <div className="p-8 flex justify-between font-bold text-white">
+    <h4 className="pr-25">Chatbots</h4>
+    <h4 className="pl-25">Knowledgebase</h4>
+    <h4 className="pr-8">Education</h4>
+    </div>
+
+    <div className="flex justify-between text-color-text">
+    <div className="p-8">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </div>
+    <div className="p-8">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</div>
+    <div className="p-8">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default GptSection
