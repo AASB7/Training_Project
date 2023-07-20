bc12f4ecc807f408d6b3b799bd6512ed5d7c3612
HanenAljadani <haneenabdullah66@hotmail.com>
2023-07-20 13:06:55 +0300

    Clean the code, and change/add some tags

--- components/GptSection.jsx
+++ components/GptSection.jsx
@@ -3,37 +3,42 @@ function GptSection() {
     <>
     <div className="m-10  inline-block  bg-gradient-to-br from-color-blog to-blue-400">
 
-    <span className="flex justify-between">
-    <div className="pl-8 pt-8 text-3xl font-bold text-white w-1/2">What is GPT-3</div>
-    
-    <div className="pl-3 pt-9 pr-7 text-xs w-1/2 text-color-text">We so opinion friends me message as delight. 
+    <div className="flex justify-between">
+    <h2 className="pl-8 pt-8 	text-3xl font-bold text-white w-1/2">What is GPT-3</h2>
+    <p className="pl-3 pt-9 pr-7 text-xs w-1/2 text-color-text">We so opinion friends me message as delight. 
     Whole front do of plate heard oh ought. His defective nor convinced residence own. 
     Connection has put impossible own apartments boisterous.
      At jointure ladyship an insisted so humanity he. 
-     Friendly bachelor entrance to on by.</div>
-    </span>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+     Friendly bachelor entrance to on by.</p>
+    </div>
+    
+    <div>
     <span className="p-8 flex justify-between">
     <h3 className=" text-3xl bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text text-transparent w-1/3">The possibilities are beyond your imagination</h3>
     <h6 className=" text-orange-400 text-sm ">Explore The Library</h6>
     </span>
+    </div>
     
-    <br/>
-    <br/>
-    <div className="p-8 flex justify-between font-bold text-white">
-    <h4 className="pr-25">Chatbots</h4>
-    <h4 className="pl-25">Knowledgebase</h4>
-    <h4 className="pr-8">Education</h4>
+
+
+    <div className="p-8 flex justify-between ">
+    <div className="">
+    <h4 className="font-bold text-white">Chatbots</h4>
+    <p className="p-8 sm:pl-2 text-color-text">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
     </div>
 
-    <div className="flex justify-between text-color-text">
-    <div className="p-8">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </div>
-    <div className="p-8">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</div>
-    <div className="p-8">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</div>
+    <div className="">
+    <h4 className="font-bold text-white">Knowledgebase</h4>
+    <p className="p-8 sm:pl-2 text-color-text">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b </p>
     </div>
+
+    <div className="">
+    <h4 className="font-bold text-white">Education</h4>
+    <p className="p-8 sm:pl-2 text-color-text">At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b </p>
+    </div>
+    </div>
+    
+
     </div>
     </>
   )
