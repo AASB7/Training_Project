344fda35745918ffadc9969d6a9b325fd78ff19e
aryam alharbi <aryamsultan45a@gmail.com>
2023-07-23 18:45:00 +0300

    new project

---
+++ components/info.jsx
@@ -0,0 +1,35 @@
+function Info() {
+  return (
+    <>
+      <div className="nav-container">
+      <nav >
+        <ul >
+          <li className="gpt"><a href="#">GPT-3</a></li>
+          <li><a href="#">HOME</a>  </li>
+          <li> <a href="#">what is GPT?</a></li>
+          <li> <a href="#">open Al</a> </li>
+          <li> <a href="#">case studies</a> </li>
+          <li> <a href="#">Library</a></li>
+        </ul>
+      </nav>
+      <button className="sign"> sign in</button>
+      <button>login </button>
+    </div>
+    <img src="images/ai.png"></img>
+    <div  >
+      <h2 >Let's Build something <br/> amazing with GPT-3 openAl </h2>
+      <p>Yet bad any for travelling assistance indulgence unpleassing not throughts all exercise blessing way <br/>
+       everything joy altereation boisterous the attachment  party<br/> we years to order allow asked of. </p>
+      <button className="email">enter your email </button>
+    <button className="Get">Get started </button>
+</div>
+
+<div className="peoples">
+ <p className="p1">1,600 poples requested access a visit in last 24 hours  </p>
+ <img src="images/people.png" className="people1"></img>
+</div>
+
+    </>
+  );
+}
+export default Info
