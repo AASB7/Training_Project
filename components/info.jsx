7dcf1ab2996b77be5e48b5727c6b4509dfe43a73
aryam alharbi <aryamsultan45a@gmail.com>
2023-07-27 11:46:17 +0300

    new

--- components/info.jsx
+++ components/info.jsx
@@ -1,8 +1,8 @@
 function Info() {
   return (
     <>
-      <div className="nav-container">
-      <nav >
+      
+    <nav>
         <ul >
           <li className="gpt"><a href="#">GPT-3</a></li>
           <li><a href="#">HOME</a>  </li>
@@ -14,19 +14,20 @@ function Info() {
       </nav>
       <button className="sign"> sign in</button>
       <button>login </button>
-    </div>
+    
     <img src="images/ai.png"></img>
     <div  >
-      <h2 >Let's Build something <br/> amazing with GPT-3 openAl </h2>
-      <p>Yet bad any for travelling assistance indulgence unpleassing not throughts all exercise blessing way <br/>
-       everything joy altereation boisterous the attachment  party<br/> we years to order allow asked of. </p>
+      <h2 >Let's Build something <br/> amazing with GPT-3 <br/>openAl </h2>
+      <p>Yet bad any for travelling assistance indulgence unpleassing not <br/>throughts all exercise blessing  
+        way everything joy altereation boisterous <br/>the attachment  party we years to order allow asked of. </p>
       <button className="email">enter your email </button>
     <button className="Get">Get started </button>
 </div>
 
 <div className="peoples">
+  <img src="images/people.png" className="people1"></img>
  <p className="p1">1,600 poples requested access a visit in last 24 hours  </p>
- <img src="images/people.png" className="people1"></img>
+ 
 </div>
 
     </>
