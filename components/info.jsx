f734ae018b1491236f8a88bdbccde50938289edc
aryam alharbi <aryamsultan45a@gmail.com>
2023-07-30 14:11:58 +0300

    new

--- components/info.jsx
+++ components/info.jsx
@@ -2,39 +2,27 @@ function Info() {
   return (
     <>
       
-    <nav>
-        <ul >
-          <li className="gpt"><a href="#">GPT-3</a></li>
-          <li><a href="#">HOME</a>  </li>
-          <li> <a href="#">what is GPT?</a></li>
-          <li> <a href="#">open Al</a> </li>
-          <li> <a href="#">case studies</a> </li>
-          <li> <a href="#">Library</a></li>
-        </ul>
-      </nav>
-      <button className="sign"> sign in</button>
-      <button>login </button>
-    
-    <div className="home">
-    <img src="images/ai.png"></img>
-    
-      <h2 >Let's Build something <br/> amazing with GPT-3 <br/>openAl </h2>
-      <p>Yet bad any for travelling assistance indulgence unpleassing<br/> not throughts all exercise blessing  
+  <div className="justify-center  text-center  ">
+      <img src="images/ai.png"></img>
+      </div> 
+      <div className="justify-center  text-center">
+      <h2 className="h2 ">Let's Build something <br/> amazing with GPT-3 <br/>openAl </h2>
+      <p >Yet bad any for travelling assistance indulgence unpleassing<br/> not throughts all exercise blessing  
       <br/>  way everything joy altereation boisterous the attachment  party<br/> we years to order allow asked of. </p>
-      <button className="email">Your Email Address </button>
-    <button className="Get">Get started </button>
+      <input type="text" className="email  " value={'enter your email'} ></input>
+     <button   className="Get "> Get started </button>
 </div>
 
-<div className="peoples">
-  <img src="images/people.png" className="people1"></img>
+<div className="peoples justify-center text-center gap-1"> 
+  <img src="images/people.png" className="people1 text-center"></img>
  <p className="p1">1,600 poples requested access a visit in last 24 hours  </p>
 </div>
- <div  className="images">
-  <img src="images/google.png"></img>
-  <img src="images/slack.png"></img>
-  <img src="images/atlassian.png"></img>
-  <img src="images/dropbox.png"></img>
-  <img src="images/shopify.png"></img>
+ <div  className=" flex justify-center items-center gap-3   ">
+  <img  className="w-20" src="images/google.png"></img>
+  <img className="w-20"  src="images/slack.png"></img>
+  <img className="w-20"  src="images/atlassian.png"></img>
+  <img className="w-20"  src="images/dropbox.png"></img>
+  <img  className="w-20"  src="images/shopify.png"></img>
  </div>
     </>
   );
